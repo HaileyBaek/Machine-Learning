14fbb8e7ac3fe10724ddd25ded6cd7103d741585
Hailey Baek <42476167+HaileyBaek@users.noreply.github.com>
2019-10-20 17:35:36 -0400

    Add files via upload

---
+++ stop_list ML 2.py
@@ -0,0 +1,14 @@
+closed_class_stop_words = ['a','the','an','and',\
+                           'as','at','for','from','in','into','of','on',\
+                           'onto','to',\
+                           'that',\
+                           'have','had','has','having','be',\
+                           'is','am','are','was','were','being','been',\
+                           'your' 'those', 'this', 'these', \
+                           'their', 'the', 'that', 'our', 'my',\
+                           'its', 'his' 'her'\
+                           'an', 'a', 'he',\
+                           'him', 'his', 'her', 'she', 'it', 'they', 'them', 'its', 'their','theirs',\
+                           'you','your','yours','me','my','mine','I','we','and/or'
+                           ]
+
